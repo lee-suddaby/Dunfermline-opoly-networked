1059c97513224590d2bfdbdc9095b5a47ec05062
Lee Suddaby <leesuddaby@gmail.com>
2020-07-19 20:37:24 +0100

    Renaming of attributes in local_game.py

--- cls_net/local_game.py
+++ cls_net/local_game.py
@@ -9,11 +9,11 @@ from .local_player import LocalPlayer
 #Stores images and other objects that cannot be passed over a network via Pyro4.
 class LocalGame:
     def __init__(self, new_die, new_PL, new_CC, new_board, new_players):
-        self.local_die = np.array(new_die) #Array of loaded pygame images
-        self.local_PL = np.array(new_PL)
-        self.local_CC = np.array(new_CC)
-        self.local_board = new_board
-        self.local_players = new_players
+        self.die = np.array(new_die) #Array of loaded pygame images
+        self.PL_Deck = np.array(new_PL)
+        self.CC_Deck = np.array(new_CC)
+        self.board = new_board
+        self.players = new_players
 
     def getDieImg(self, score, die_num):
         return self.local_die[die_num].getImg(score)
